94f5cd5e150414e5670396bc95be7a518053ccbb
MelvinPeepers <peepersm@gmail.com>
2019-08-31 14:25:36 -0400

    setup most of migrations

--- data/migrations/20190831134050_create-tables.js
+++ data/migrations/20190831134050_create-tables.js
@@ -2,15 +2,44 @@
 exports.up = function(knex) {
   return knex.schema.createTables('projects', tbl => {
       tbl.increments();
-      tbl.string('animal_name', 256).notNullable();
-      tbl.string('resource_name');
-      tbl.boolean('project_completed');
+      tbl.string('project_name', 256).notNullable();
+      tbl.string('project_description', 256);
+      tbl.boolean('project_completed').defaultTo(false);
   })
   .createTable('resources', tbl => {
-      
+      tbl.increments();
+      tbl.string('resource_name', 256).notNullable();
+      tbl.string('resource_description', 256);
+  })
+  .createTable('tasks', tbl => {
+      tbl.increments();
+      tbl.string('task_description', 256).notNullable();
+      tbl.string('task_notes', 512);
+      tbl.boolean('task_completed').defaultTo(false);
+      tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      // this table must exist
+      .inTable('projects')
+  })
+  // Intermediate table
+  .createTable('project_resources', tbl => {
+      tbl.integer('resource_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+    // this table must exist
+      .inTable('resources')
   })
 };
 
 exports.down = function(knex) {
-  
+  // reverse order of creation
+  return knex.schema
+  .dropTableIfExisit('tasks')
+  .dropTableIfExisit('resources')
+  .dropTableIfExisit('projects');
 };
+
+// http://knexjs.org/#Schema-defaultTo defaultTo - Sets the default value for the column on an insert.
