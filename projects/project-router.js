8696a80f93e7f31ce72276089e4c30666f21eac6
MelvinPeepers <peepersm@gmail.com>
2019-08-31 14:59:15 -0400

    Made some quick and endpoints

--- projects/project-router.js
+++ projects/project-router.js
@@ -14,4 +14,35 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    Projects.findById(id)
+    .then(project => {
+  
+      if (user) {
+        res.json(project);
+      } else {
+        res.status(404).json({ message: 'Could not find project with given id.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get project' });
+    });
+  });
+
+  router.post('/', (req, res) => {
+    const projectData = req.body;
+  
+    Users.add(projectData)
+    .then(newProject => {
+      res.status(201).json(newProject);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to create new project' });
+    });
+  });
+  
+  
+
 module.exports = router;
