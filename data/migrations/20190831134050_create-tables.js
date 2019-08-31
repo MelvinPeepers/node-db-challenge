6c3cc96384b544f6ed202604e0e5b20b03339e6a
MelvinPeepers <peepersm@gmail.com>
2019-08-31 13:52:30 -0400

    added projects to migrations

--- data/migrations/20190831134050_create-tables.js
+++ data/migrations/20190831134050_create-tables.js
@@ -1,6 +1,14 @@
 
 exports.up = function(knex) {
-  return knex.schema
+  return knex.schema.createTables('projects', tbl => {
+      tbl.increments();
+      tbl.string('animal_name', 256).notNullable();
+      tbl.string('resource_name');
+      tbl.boolean('project_completed');
+  })
+  .createTable('resources', tbl => {
+      
+  })
 };
 
 exports.down = function(knex) {
