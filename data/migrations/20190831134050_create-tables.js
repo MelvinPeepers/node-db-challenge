ddd1a2957eab0071da125c1e4b265e2f710abba4
MelvinPeepers <peepersm@gmail.com>
2019-08-31 14:40:58 -0400

    think i'm done with my migration

--- data/migrations/20190831134050_create-tables.js
+++ data/migrations/20190831134050_create-tables.js
@@ -16,7 +16,6 @@ exports.up = function(knex) {
       tbl.string('task_description', 256).notNullable();
       tbl.string('task_notes', 512);
       tbl.boolean('task_completed').defaultTo(false);
-      // (foreign keys)
       tbl.integer('project_id') 
       .unsigned()
       .notNullable()
@@ -24,7 +23,7 @@ exports.up = function(knex) {
       // this table must exist
       .inTable('projects')
   })
-  // Intermediate table
+  // Intermediate table (foreign keys)
   .createTable('project_resources', tbl => {
       tbl.integer('project_id')
       .unsigned()
@@ -47,6 +46,7 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   // reverse order of creation
   return knex.schema
+  .dropTableIfExisit('project_resources')
   .dropTableIfExisit('tasks')
   .dropTableIfExisit('resources')
   .dropTableIfExisit('projects');
@@ -54,46 +54,3 @@ exports.down = function(knex) {
 
 // http://knexjs.org/#Schema-defaultTo defaultTo - Sets the default value for the column on an insert.
 
-exports.up = function(knex) {
-    return knex.schema.createTable('zoos', tbl => {
-      tbl.increments();
-      tbl.string('zoo_name', 128).notNullable();
-      tbl.string('address', 128).notNullable().unique();
-    })
-    .createTable('species', tbl => {
-      tbl.increments();
-      tbl.string('species_name', 128).notNullable().unique();
-    })
-    .createTable('animals', tbl => {
-      tbl.increments();
-      tbl.string('animal_name', 128).notNullable();
-      // foreign key setup using knex
-      tbl.integer('species_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        // this table must exist
-        .inTable('species')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
-    })
-    .createTable('zoo_animals', tbl => {
-      tbl.integer('zoo_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        // this table must exist
-        .inTable('zoos')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
-      tbl.integer('animal_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        // this table must exist
-        .inTable('animals')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
-      tbl.primary(['zoo_id', 'animal_id']);
-    });
-  };
